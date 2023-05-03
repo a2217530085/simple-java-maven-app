3b4ea5aa2493c2c91bb897ae8986bac50a735548
a2217530085 <66411080+a2217530085@users.noreply.github.com>
2023-05-03 21:43:51 +0800

    测试运行结果1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,6 +3,7 @@ package com.mycompany.app;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 /**
  * Hello world!
@@ -13,22 +14,29 @@ public class App {
 
     public App() {}
 
-    public static void main(String[] args) throws IOException {
-        File file = new File("/java");
-        if (!file.exists()) {
-            file.mkdir();
+    public static void main(String[] args) {
+        File file = new File("/java/sout.log");
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(file);
+            PrintStream ps = new PrintStream(fos);
+            System.setOut(ps);
+            System.out.println(MESSAGE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        file = new File("/java/sout.log");
-        if (!file.exists()) {
-            file.createNewFile();
-            FileOutputStream outputStream = new FileOutputStream(file);
-            outputStream.write(MESSAGE.getBytes());
-            outputStream.close();
-        }
-        System.out.println(MESSAGE);
     }
 
     public String getMessage() {
         return MESSAGE;
     }
 }
+
