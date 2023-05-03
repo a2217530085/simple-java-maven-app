03b54a90f87cdfc8a124b9559a363207c1e649be
刘泽伟 <2217530085@qq.com>
2023-05-03 21:22:23 +0800

    测试运行结果

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,9 @@
 package com.mycompany.app;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Hello world!
  */
@@ -9,7 +13,18 @@ public class App {
 
     public App() {}
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File file = new File("/java");
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        file = new File("/java/sout.log");
+        if (!file.exists()) {
+            file.createNewFile();
+            FileOutputStream outputStream = new FileOutputStream(file);
+            outputStream.write(MESSAGE.getBytes());
+            outputStream.close();
+        }
         System.out.println(MESSAGE);
     }
 
